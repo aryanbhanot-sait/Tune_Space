395638ae34bdfd83e518b57833450487c68d58f7
Aryan Bhanot <Aryan.Bhanot@edu.sait.ca>
2025-07-16 17:56:38 -0600

    .env implementation

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://zazneocfcpjltirxgxui.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inphem5lb2NmY3BqbHRpcnhneHVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2NDA3MTUsImV4cCI6MjA2ODIxNjcxNX0._oOjczqBRB4UJ_IYIuCHgaNL9yKXAtP4tI9pux-7GaE';
+const SUPABASE_URL = process.env.EXPO_PUBLIC_URL || '';
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
