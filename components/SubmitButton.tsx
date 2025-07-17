1b950c4cb4d010afd472fd76f0169c44be29d9b3
voraingautam <vorain.gautam@edu.sait.ca>
2025-07-16 19:02:58 -0600

    Update SubmitButton.tsx

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,1 +1,16 @@
-// here
+import React from 'react';
+import { Button, View } from 'react-native';
+
+interface Props {
+  title: string;
+  onPress: () => void;
+  disabled?: boolean;
+}
+
+export default function SubmitButton({ title, onPress, disabled }: Props) {
+  return (
+    <View style={{ width: '100%', marginBottom: 10 }}>
+      <Button title={title} onPress={onPress} color="#1DB954" disabled={disabled} />
+    </View>
+  );
+}// here
